4082ded656eb8646564ab6573872748e1ecccc01
Marco Rozzati <marco.rozzati@mondora.com>
2021-10-28 10:41:55 +0200

    update package name

--- iris/adapter.go
+++ iris/adapter.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/awslabs/aws-lambda-go-api-proxy/core"
+	"github.com/mondora/aws-lambda-go-api-proxy/core"
 	"github.com/kataras/iris/v12"
 )
 
